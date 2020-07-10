fc9fdee1254a74fdef7c8df228987e19aa25e853
scofield <shigf2012@gmail.com>
2020-07-10 09:45:41 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -16,17 +16,8 @@ def start():
     referer = 'https://glados.rocks/console/checkin'
     checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer })
     state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer})
-    #print(res)
-
-    if 'message' in checkin.text:
-        mess = checkin.json()['message']
-        time = state.json()['data']['leftDays']
-        time = time.split('.')[0]
-        print(time)
-        if sever == 'on':
-            requests.get('https://sc.ftqq.com/' + sckey + '.send?text='+mess+'，you have '+time+' days left')
-    else:
-        requests.get('https://sc.ftqq.com/' + sckey + '.send?text=cookie过期')
+    print(checkin)
+    print(state)
 
 def main_handler(event, context):
   return start()
