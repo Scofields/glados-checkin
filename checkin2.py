ecc38ef4067793c16cba96f7859f31d8bc03db80
scofield <shigf2012@gmail.com>
2020-09-18 16:24:17 +0800

    Create checkin2.py

---
+++ checkin2.py
@@ -0,0 +1,26 @@
+import requests,json,os
+
+# server酱开关，填off不开启(默认)，填on同时开启cookie失效通知和签到成功通知
+sever = os.environ["SERVE"]
+
+# 填写server酱sckey,不开启server酱则不用填
+sckey = os.environ["SCKEY"]
+
+# 填入glados账号对应cookie
+cookie = os.environ["COOKIE2"]
+
+
+def start():    
+    url= "https://glados.rocks/api/user/checkin"
+    url2= "https://glados.rocks/api/user/status"
+    referer = 'https://glados.rocks/console/checkin'
+    checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer })
+    state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer})
+    print(checkin)
+    print(state)
+
+def main_handler(event, context):
+  return start()
+
+if __name__ == '__main__':
+    start()
